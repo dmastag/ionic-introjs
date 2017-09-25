40c26499875cbc6d0c0cac843f5b59039de3e5cd
Julian GM Alimin <dmastag@yahoo.com>
2017-09-25 13:54:21 +0700

    implement introjs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import introJs from 'intro.js/intro.js';
+
+declare var introJs: any;
 
 @Component({
   selector: 'page-home',
@@ -8,7 +11,32 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    console.log(introJs.introJs());
+  }
 
+  intro() {
+
+    let intro = introJs.introJs();
+    intro.setOptions({
+      steps: [{
+        element: '#step1',
+        intro: 'Step one description',
+        tooltipPosition: 'bottom'
+      },
+      {
+        element: '#step2',
+        intro: 'Step <i>two</i> description',
+        tooltipPosition: 'bottom'
+      },
+      {
+        element: '#step3',
+        intro: 'Step <span style="color: green;">three</span> description',
+        tooltipPosition: 'bottom'
+      }]
+    });
+
+    // Start tutorial
+    intro.start();
   }
 
 }
